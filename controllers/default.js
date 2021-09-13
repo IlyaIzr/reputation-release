ec2a8fb81f76193600683bf5b4df83dba296ca2f
ilyaizr <ilyaizrailyan@gmail.com>
2021-09-13 20:16:33 +0300

    ADD - Certificate fix

--- controllers/default.js
+++ controllers/default.js
@@ -1,7 +1,17 @@
+var fs = require("fs");
+
 exports.install = function () {
 	ROUTE('GET /*', mainPage);
+
+	ROUTE("GET /.well-known/acme-challenge/", cert);
 };
 
 function mainPage() {
 	this.view('layout')
+}
+
+
+function cert() {
+	// this === framework
+	this.stream("text/plain", fs.createReadStream(PATH.public(this.url)));
 }
