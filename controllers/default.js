e225ade6701671c2e7511fbd83d736f235ff8bb9
Vivtonenko Alexandr <dispbd@gmail.com>
2021-09-14 14:29:12 +0300

    fix cert

--- controllers/default.js
+++ controllers/default.js
@@ -1,18 +1,18 @@
 var fs = require("fs");
 
-exports.install = function () {
-	ROUTE('GET /*', mainPage);
+exports.install = function() {
+    ROUTE('GET /*', mainPage);
 
-	ROUTE("GET /.well-known/acme-challenge/", cert);
+    ROUTE("GET /.well-known/acme-challenge/*", cert);
 };
 
 function mainPage() {
-	this.view('layout')
+    this.view('layout')
 }
 
 
 function cert() {
-	// this === framework
-  // PATH.mkdir(PATH.public(this.url), '');
-	this.stream("text/plain", fs.createReadStream(PATH.public(this.url)));
+    // this === framework
+    // PATH.mkdir(PATH.public(this.url), '');
+    this.stream("text/plain", fs.createReadStream(PATH.public(this.url)));
 }
