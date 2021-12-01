c7b316459f1a184fc5bbf0c47e61af5d7a3750ec
Vivtonenko Alexandr <dispbd@gmail.com>
2021-12-01 23:48:00 +0300

    fix LinkButton createFund

--- client/src/LinkButton.svelte
+++ client/src/LinkButton.svelte
@@ -1,19 +1,19 @@
 <script>
-import { goTo, location } from "./rest/store";
-// props
-export let to
-export let label = to;
-let current = false
+  import { goTo, location } from "./rest/store";
+  // props
+  export let to;
+  export let label = to;
+  let current = false;
 
-function onClick() {
-  current = !current
-  goTo('/' + to)
-}
-location.subscribe(loc => {
-  if (loc.substring(1) === to) current = true
-  else current = false
-})
-;
+  function onClick() {
+    current = !current;
+    goTo("/" + to);
+  }
+  
+  location.subscribe((loc) => {
+    if (loc.substring(1) === to) current = true;
+    else current = false;
+  });
 </script>
 
 <button class="link-button ui button" class:current on:click={onClick}>{label || to}</button>
@@ -26,5 +26,4 @@ location.subscribe(loc => {
     text-decoration: underline;
     pointer-events: none;
   }
-
 </style>
